6f61f026edf5b5fb55b461442de9a1b55f8dcde5
mrzhuzhe <466648706@qq.com>
2024-11-12 23:56:34 +0800

    wire

--- ReadMe.md
+++ ReadMe.md
@@ -10,3 +10,4 @@
 1. Static Timing Analysis for Nanometer Designs: A Practical Approach
 2. advanced digital design with the verilog hdl
 3. https://hdlbits.01xz.net/wiki/Main_Page
+4. https://ciechanow.ski/
--- basiec/test.v
+++ basiec/test.v
@@ -1,6 +1,10 @@
-module top_module( output one );
+module top_module( output one , output zero, input a, input b, output c, output d );
 
 // Insert your code here
     assign one = 1'b1;
-
+    assign zero = 1'b0;
+    //  nor gate
+    assign c = ~(a&&b);
+    //  xnor gate
+    assign d = ~(a^b);
 endmodule
