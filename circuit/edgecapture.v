82e3d94128722b6efcb39a147ee8199046284266
mrzhuzhe <466648706@qq.com>
2024-11-27 20:58:59 +0800

    edge capture

--- circuit/edgecapture.v
+++ circuit/edgecapture.v
@@ -1,23 +1,3 @@
-// module top_module (
-//     input clk,
-//     input reset,
-//     input [31:0] in,
-//     output [31:0] out
-// );
-//     reg [31:0] in_d;
-//     reg [31:0] reset_d =2'h00;
-//     always @(posedge clk) begin
-//         in_d <= in;
-//         if (reset) begin
-//             out <= 2'h00;
-//         end
-//         else begin
-//             if (in_d & ~in) out <= in_d & ~in;
-//         end 
-//     end
-// endmodule
-
-
 module top_module (
     input clk,
     input reset,
@@ -32,10 +12,8 @@ module top_module (
             out<=32'b0;
         end
         else begin
-            for(i=0;i<32;i=i+1)begin
-                if(temp[i] & ~in[i])begin
-                    out[i]<= temp[i] & ~in[i];
-                end
+            if(temp & ~in)begin
+                out <= out | (temp & ~in);
             end
         end
     end
